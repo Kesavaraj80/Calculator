29161851459b03172c49aa2ffac36783c2828fe2
Kesavaraj08 <mkesavaraj1998@gmail.com>
2021-08-28 11:59:58 +0530

    calc final change

--- script.js
+++ script.js
@@ -3,35 +3,43 @@ main_container.setAttribute('class','main-box');
 document.body.append(main_container);
 main_container.innerHTML=`
 <input type="text" class="input1" id="box1">
-<div class="buttons">
-    <button class="btn" onclick="clr()">CL</button>
-    <button class="btn" onclick="del()">DEL</button>
-    <button class="btn" onclick="display('%')">%</button>
-    <button class="btn" onclick="display('/')">/</button>
-</div>
-<div class="buttons">
-    <button class="btn" onclick="display('9')">9</button>
-    <button class="btn" onclick="display('8')">8</button>
-    <button class="btn" onclick="display('7')">7</button>
-    <button class="btn" onclick="display('*')">*</button>
-</div>
-<div class="buttons">
-    <button class="btn" onclick="display('6')">6</button>
-    <button class="btn" onclick="display('5')">5</button>
-    <button class="btn" onclick="display('4')">4</button>
-    <button class="btn" onclick="display('-')">-</button>
-</div>
-<div class="buttons">
-    <button class="btn" onclick="display('3')">3</button>
-    <button class="btn" onclick="display('2')">2</button>
-    <button class="btn" onclick="display('1')">1</button>
-    <button class="btn" onclick="display('+')">+</button>
-</div>
-<div class="buttons">
-    <button class="btn" onclick="display('0')">0</button>
-    <button class="btn" onclick="display('00')">00</button>
-    <button class="btn" onclick="display('.')">.</button>
-    <button class="btn" onclick="calc()">=</button>
+<div class="btns">
+    <div class="buttons">
+        <button class="btn" onclick="clr()">CL</button>
+        <button class="btn" onclick="del()">DEL</button>
+        <button class="btn" onclick="display('(')">(</button>
+        <button class="btn" onclick="display(')')">)</button>
+    </div>
+    <div class="buttons">
+        <button class="btn" onclick="power2()">X<sup>2</sup></button>
+        <button class="btn" onclick="power3()">X<sup>3</sup></button>
+        <button class="btn" onclick="display('%')">%</button>
+        <button class="btn" onclick="display('/')">/</button>
+    </div>
+    <div class="buttons">
+        <button class="btn" onclick="display('9')">9</button>
+        <button class="btn" onclick="display('8')">8</button>
+        <button class="btn" onclick="display('7')">7</button>
+        <button class="btn" onclick="display('*')">*</button>
+    </div>
+    <div class="buttons">
+        <button class="btn" onclick="display('6')">6</button>
+        <button class="btn" onclick="display('5')">5</button>
+        <button class="btn" onclick="display('4')">4</button>
+        <button class="btn" onclick="display('-')">-</button>
+    </div>
+    <div class="buttons">
+        <button class="btn" onclick="display('3')">3</button>
+        <button class="btn" onclick="display('2')">2</button>
+        <button class="btn" onclick="display('1')">1</button>
+        <button class="btn" onclick="display('+')">+</button>
+    </div>
+    <div class="buttons">
+        <button class="btn" onclick="display('0')">0</button>
+        <button class="btn" onclick="display('00')">00</button>
+        <button class="btn" onclick="display('.')">.</button>
+        <button class="btn" onclick="calc()">=</button>
+    </div>
 </div>`
 
 function display(num){
@@ -50,4 +58,12 @@ function calc(){
 }
 function clr(){
     document.getElementById('box1').value="";
+}
+function power2(){
+    let pval2 = document.getElementById('box1').value;
+    document.getElementById('box1').value=Math.pow(pval2,2).toFixed(2);
+}
+function power3(){
+    let pval3 = document.getElementById('box1').value;
+    document.getElementById('box1').value=Math.pow(pval3,3).toFixed(2);
 }
